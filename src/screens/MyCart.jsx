21e9ea3551dd996e6a1ed6f87fb25263a66d0c3e
vedataltun <rttvedudi@gmail.com>
2024-12-10 05:22:33 +0300

    bug fixed and new page filled

--- src/screens/MyCart.jsx
+++ src/screens/MyCart.jsx
@@ -1,7 +1,36 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Items} from '../database/Database';
+import {useNavigation} from '@react-navigation/native';
+import { Colors } from '../themes/Colors';
 
 const MyCart = () => {
+  const [product, setProduct] = useState([]);
+  const [total, setTotal] = useState(0);
+  const navigation = useNavigation();
+  useEffect(() => {
+    getDataFromDB();
+  }, [navigation]);
+  const getDataFromDB = async () => {
+    let items = await AsyncStorage.getItem('cartItems');
+    items = JSON.parse(items);
+    console.log(items);
+    
+    let productData = [];
+    if (items) {
+      Items.forEach(data => {
+        if (items.includes(data.id)) {
+          data.quantity = 1;
+          productData.push(data);
+        }
+      });
+      setProduct(productData);
+    } else {
+      setProduct([]);
+      setTotal(0);
+    }
+  };
   return (
     <View>
       <Text>MyCart</Text>
@@ -11,4 +40,53 @@ const MyCart = () => {
 
 export default MyCart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 16,
+    width: '62%',
+    alignItems: 'center',
+  },
+  backButton: {
+    backgroundColor: Colors.backgroundLight,
+    padding: 12,
+    borderRadius: 12,
+    marginLeft: 15,
+  },
+  orderTitle: {
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: '400',
+  },
+  myCart: {
+    fontSize: 20,
+    color: Colors.black,
+    paddingTop: 20,
+    paddingLeft: 16,
+    marginBottom: 10,
+    fontWeight: '500',
+  },
+  locationText: {
+    fontSize: 16,
+    color: Colors.black,
+    paddingHorizontal: 16,
+    marginVertical: 10,
+    marginBottom: 10,
+    letterSpacing: 1,
+    fontWeight: '500',
+  },
+  deliveryBox: {
+    backgroundColor: Colors.backgroundLight,
+    padding: 12,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  creditCard: {
+    backgroundColor: Colors.backgroundLight,
+    padding: 12,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/screens/ProductInfo.jsx
+++ src/screens/ProductInfo.jsx
@@ -13,24 +13,42 @@ import React, {useEffect, useState} from 'react';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {Items} from '../database/Database';
 import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import {Colors} from '../themes/Colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ProductInfo = () => {
-  const scrollX = new Animated.Value(10);
-  const position =Animated.divide(scrollX,width)
   const width = Dimensions.get('window').width;
+  const scrollX = new Animated.Value(10);
+  const position = Animated.divide(scrollX, width);
   const navigation = useNavigation();
   const [product, setProduct] = useState([]);
   const route = useRoute();
   const {productID} = route.params;
   useEffect(() => {
     getDataFromDB();
-  }, []);
+  }, [navigation]);
   const getDataFromDB = () => {
     const product = Items.find(item => item.id === productID);
     if (product) {
       setProduct(product);
       return;
+    }
+  };
+  const addToCart = async id => {
+    let itemArray = await AsyncStorage.getItem('cartItems');
+    itemArray = JSON.parse(itemArray);
+    let array = itemArray ? itemArray : [];
+    array.push(id);
+    // console.log("array",array);
+    // console.log("itemarray",itemArray);
+    
+    try {
+      await AsyncStorage.setItem('cartItems', JSON.stringify(array));
+      navigation.navigate('Home');
+    } catch (error) {
+      return error;
     }
   };
 
@@ -94,11 +112,11 @@ const ProductInfo = () => {
             }}>
             {product.productImageList
               ? product.productImageList.map((data, index) => {
-                let opacity=position.interpolate({
-                    inputRange:[index-1,index,index+1],
-                    outputRange:[0.2,1,0.2],
-                    extrapolate:"clamp"
-                })
+                  let opacity = position.interpolate({
+                    inputRange: [index - 1, index, index + 1],
+                    outputRange: [0.2, 1, 0.2],
+                    extrapolate: 'clamp',
+                  });
                   return (
                     <Animated.View
                       key={index}
@@ -107,14 +125,143 @@ const ProductInfo = () => {
                         height: 2.4,
                         backgroundColor: Colors.black,
                         marginHorizontal: 4,
-                        opacity
+                        opacity,
                       }}></Animated.View>
                   );
                 })
               : null}
           </View>
+          <View style={{paddingHorizontal: 16, marginTop: 6}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginVertical: 14,
+              }}>
+              <Entypo
+                name="shopping-cart"
+                style={{
+                  fontSize: 18,
+                  color: Colors.blue,
+                  marginRight: 6,
+                }}
+              />
+              <Text style={{color: Colors.black, fontSize: 12}}>Shopping</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginVertical: 4,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: '600',
+                  letterSpacing: 0.4,
+                  color: Colors.black,
+                  maxWidth: '84%',
+                  marginVertical: 4,
+                }}>
+                {product.productName}
+              </Text>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: Colors.blue + 20,
+                  padding: 8,
+                  borderRadius: 100,
+                }}>
+                <Ionicons name="link-outline" size={24} color={Colors.black} />
+              </TouchableOpacity>
+            </View>
+            <Text
+              style={{
+                fontSize: 12,
+                color: Colors.black,
+                fontWeight: '400',
+                maxWidth: '85%',
+                opacity: 0.5,
+                lineHeight: 20,
+                maxHeight: 44,
+                marginBottom: 18,
+              }}>
+              {product.description}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+
+                justifyContent: 'space-between',
+              }}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View
+                  style={{
+                    backgroundColor: Colors.backgroundLight,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 12,
+                    marginRight: 10,
+                    borderRadius: 100,
+                  }}>
+                  <Entypo name="location-pin" size={16} color={Colors.blue} />
+                </View>
+                <Text>İstanbul Üsküdar {'\n'} 17-0001</Text>
+              </View>
+              <Entypo
+                name="chevron-right"
+                size={22}
+                color={Colors.backgroundDark}
+              />
+            </View>
+          </View>
+          <View style={{paddingHorizontal: 16}}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: '500',
+                color: Colors.black,
+                marginVertical: 4,
+              }}>
+              {product.productPrice}.00 ₺
+            </Text>
+            <Text>
+              Tax Rate %2 {product.productPrice / 20}₺ ({' '}
+              {product.productPrice + product.productPrice / 20}₺ )
+            </Text>
+          </View>
         </View>
       </ScrollView>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          bottom: 0,
+          width: '100%',
+          height: '8%',
+        }}>
+        <TouchableOpacity
+          onPress={() => (product.isAvailable ? addToCart(product.id) : null)}
+          style={{
+            width: '86%',
+            height: '90%',
+            backgroundColor: Colors.blue,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: '500',
+              color: Colors.white,
+              textTransform: 'uppercase',
+            }}>
+            {product.isAvailable ? 'Add to cart' : 'Not Available'}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
